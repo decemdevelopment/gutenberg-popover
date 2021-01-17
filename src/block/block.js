ac6fdbd353db69c5713b517818416e06ac069bea
moonbyte <moonbyt3@gmail.com>
2021-01-17 17:41:21 +0100

    added custom icon for backend

--- src/block/block.js
+++ src/block/block.js
@@ -11,6 +11,8 @@ import "./style.scss";
 
 import _ from 'lodash'
 
+import { Icon } from '@wordpress/components'
+
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 
@@ -25,6 +27,15 @@ import {
   ToggleControl,
   Button,
 } from "@wordpress/components";
+
+const PopperIcon = () => (
+  <Icon
+    icon={
+      <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.286 4H5.714C4.768 4 4 4.768 4 5.714v9.143c0 .947.768 1.715 1.714 1.715h2.029l-.311 2.794a.571.571 0 00.95.487l3.647-3.282h6.257c.947 0 1.714-.767 1.714-1.714V5.714C20 4.768 19.233 4 18.286 4zm.571 10.857a.571.571 0 01-.571.572H11.81a.571.571 0 00-.383.146L8.734 18l.215-1.936a.571.571 0 00-.568-.635H5.714a.571.571 0 01-.571-.572V5.714c0-.315.256-.571.571-.571h12.572c.315 0 .571.256.571.571v9.143z" fill="#000" /></svg>
+    }
+  >
+  </Icon>
+)
 
 const PopoverButton = ({ onChange, isActive, ...props }) => {
 
@@ -104,7 +115,7 @@ const PopoverButton = ({ onChange, isActive, ...props }) => {
   return (
     <Fragment>
       <RichTextToolbarButton
-        icon="editor-code"
+        icon={PopperIcon}
         title="Enable Popover"
         onClick={handleAddingPopover}
         isActive={isActive}
